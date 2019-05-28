1d3b29d0ff4613d8ada9ffdca05c1102a7157aa2
DanicaMeng <751927231@qq.com>
2019-05-28 17:43:55 +0800

    second-submit

--- Conan.py
+++ Conan.py
@@ -3,14 +3,16 @@ import os
 
 
 def tamper(student_id):
-  #pass
+
   with open ('lenna.bmp','rb+') as f:
     f.seek(54)
+    desti=54
     for i in range(12):
-      #r=bytes(student_id[i])
-      #r=struct.unpack(student_id[i],temp)
-      #pack_data=struct.pack(student_id[i])
-      f.seek(int(student_id[i]))
+      num=int(student_id[i])
+      if num==0:
+        num=10
+      desti += num*3
+      f.seek(desti)
       f.write(b'\x00\x00\x00')
     
 def detect():
@@ -42,6 +44,6 @@ def detect():
 
 if __name__ == '__main__':
   import sys
-  tamper(sys.argv[1])
+  tamper('201811113004')
 
   detect()
